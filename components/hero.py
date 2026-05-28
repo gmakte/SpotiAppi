77a58e9f70692e829869c23c9d9790f9585a1333
maribel acer <maribelpreite@gmail.com>
2026-05-28 19:59:30 +0200

    main card fixed

--- components/hero.py
+++ components/hero.py
@@ -54,19 +54,39 @@
 
 
 import streamlit as st
+from assets.palette import USER_COLORS
 
 def render_hero(df):
 
     html = f"""
-    <div class="main-card">
+        <div class="main-card">
 
-    <div class="hero-title gradient-text">
-    Three people.<br>
-    Three musical identities.
-    </div>
-    """
+            <div class="hero-content">
 
-    st.markdown(
-        html,
-        unsafe_allow_html=True
-    )
+                <div class="hero-title">
+
+                    <span style="color:{USER_COLORS['Ashanti']}">
+                        Three people.
+                    </span>
+
+                    <span style="color:{USER_COLORS['Gabi']}">
+                        Three musical
+                    </span>
+
+                    <span style="color:{USER_COLORS['Maribel']}">
+                        identities.
+                    </span>
+
+                </div>
+
+                <div class="hero-subtitle">
+                    Exploring our listening habits,<br>
+                    similarities and personalities.
+                </div>
+
+            </div>
+
+        </div>
+        """
+
+    st.html(html)
